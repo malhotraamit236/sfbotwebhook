b2cf9bf671ef109c8584f9d5a2612052e9935b2e
malhotraamit236 <malhotraamit236@users.noreply.github.com>
2017-11-06 15:45:30 +0530

    Adding SF API call

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
+const fetch = require('node-fetch');
 
 
 router.post('/webhook', function(req, res){
-	console.log('hi');
+	/*console.log('hi');
 	console.log(req.body);
 	var sampleResponse = {
         "speech": "Hi Test",
         "displayText": "Hi Test",
         "source": "apiai-sfbot"
     };
-	res.send(JSON.stringify(sampleResponse));
+	res.send(JSON.stringify(sampleResponse));*/
+
+	var myInit = { method: 'GET',
+	               mode: 'cors',
+	               cache: 'default' };
+
+	fetch('https://staypositivebot.herokuapp.com/api', myInit).then(function(response) {
+	  return response.json();
+	}).then(function(myJson) {
+	  console.log("Hi inside webhook");
+	  res.send(myJson);
+	});
+
+
 });
 
 module.exports = router;
